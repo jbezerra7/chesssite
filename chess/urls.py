b97ef9b255de8a6365747c4335a3c3c946cc08b5
Joe <josephlamacchia1@gmail.com>
2024-01-14 14:01:47 -0500

    views

--- chess/urls.py
+++ chess/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
 from . import views
+from .views import ChessGameView
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("chess", ChessGameView.as_view(), name="chess"),
+    path('make_move/', ChessGameView.make_move, name='make_move'),
+
 ]
